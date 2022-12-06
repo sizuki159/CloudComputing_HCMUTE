dd2ac0bcc4d84b7fbb953623859003de30c53591
sizuki159 <luahantinhthu9@gmail.com>
2022-12-06 14:16:05 +0700

    Config mysql Amazon RDS

--- config.php
+++ config.php
@@ -1,3 +1,18 @@
 <?php
 
+$servername = "cloudcomputing.crghsm7cx5ro.us-east-1.rds.amazonaws.com";
+$port = 3306;
+$database = "cloudcomputing";
+$username = "root";
+$password = "Trung123";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database, $port);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+
 ?>
